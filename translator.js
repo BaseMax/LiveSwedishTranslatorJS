e82717a192730ae40a0519de1839e2120f39da8f
Max Base <MaxBaseCode@Gmail.Com>
2020-07-10 19:33:42 +0430

    Create translator.js

---
+++ translator.js
@@ -0,0 +1,15 @@
+/*
+ * @name: Live Swedish translator (web, javascript)
+ * @description: translate Swedish chars to normal english form
+ * @instructions:
+	Ä = EA
+	Ö = EO
+	Å = OO
+	ä = ea
+	å = oo
+	ö = eo
+ * @company: Lin24.se
+ * @author: Max Base (github.com/basemax)
+ * @website: maxbase.org
+ * @date: 2020-07-10
+*/
